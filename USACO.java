9a4704d0f0d11378e928fd5336280550b80fac6c
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-05 18:51:37 -0500

    created info method to get information from file

--- USACO.java
+++ USACO.java
@@ -1,3 +1,24 @@
 public class USACO{
-  
+  public int[][] lake;
+  public ArrayList<ArrayList<Integer>> instructions;
+  public void info(String name){
+    File file = new File(name);
+    Scanner text = new Scanner(file);
+    String output = text.nextLine();
+    lake = new int[Integer.parseInt(output.substring(0, 1))][Integer.parseInt(output.substring(1, 2))];
+    int counter = 0;
+    while(counter < lake.length){
+      output = text.nextLine();
+      for(int i = 0; i < output.length; i++){
+        lake[counter][i] = Integer.parseInt(output.substring(i, i + 1));}
+      counter ++;}
+    instructions = new ArrayList<ArrayList<Integer>>();
+    counter = 0;
+    while(text.hasNextLine()){
+      output = text.nextLine();
+      instructions.add(new ArrayList<Integer>());
+      counter ++;
+      for(int i = 0; i < 3; i++){
+        instructions.get(counter).add(Integer.parseInt(output.substring(i, i + 1)));}}}
+
 }
