bcfdba765550fdc63009f609d74ea84b8919b91c
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-05 19:08:32 -0500

    index out of bounds exception  at line 18

--- USACO.java
+++ USACO.java
@@ -9,12 +9,15 @@ public class USACO{
     File file = new File(name);
     Scanner text = new Scanner(file);
     String output = text.nextLine();
-    lake = new int[Integer.parseInt(output.substring(0, 1))][Integer.parseInt(output.substring(1, 2))];
+    lake = new int[Integer.parseInt(output.substring(0, 1))][Integer.parseInt(output.substring(2, 3))];
+    System.out.println(" " + lake.length + " " + lake[0].length);
     int counter = 0;
     while(counter < lake.length){
       output = text.nextLine();
       for(int i = 0; i < output.length(); i++){
-        lake[counter][i] = Integer.parseInt(output.substring(i, i + 1));}
+        try{
+        lake[counter][i / 2] = Integer.parseInt(output.substring(i, i + 1));}
+        catch(NumberFormatException e){}}
       counter ++;}
     instructions = new ArrayList<ArrayList<Integer>>();
     counter = 0;
