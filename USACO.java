c5202bbbbc289298a91d12ad4b115dee3e1900cd
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-06 11:30:27 -0500

    fixed outof bounds error at 18

--- USACO.java
+++ USACO.java
@@ -10,22 +10,21 @@ public class USACO{
     Scanner text = new Scanner(file);
     String output = text.nextLine();
     lake = new int[Integer.parseInt(output.substring(0, 1))][Integer.parseInt(output.substring(2, 3))];
-    System.out.println(" " + lake.length + " " + lake[0].length);
     int counter = 0;
     while(counter < lake.length){
-      output = text.nextLine();
-      for(int i = 0; i < output.length(); i++){
-        try{
-        lake[counter][i / 2] = Integer.parseInt(output.substring(i, i + 1));}
-        catch(NumberFormatException e){}}
-      counter ++;}
-    instructions = new ArrayList<ArrayList<Integer>>();
-    counter = 0;
-    while(text.hasNextLine()){
-      output = text.nextLine();
-      instructions.add(new ArrayList<Integer>());
+      for(int i = 0; i < lake[0].length; i++){
+        lake[counter][i] = text.nextInt();}
       counter ++;
-      for(int i = 0; i < 3; i++){
-        instructions.get(counter).add(Integer.parseInt(output.substring(i, i + 1)));}}}
+    }}
+  public String toString(){
+    String output = "";
+    for(int i = 0; i < lake.length; i++){
+      for(int j = 0; j < lake[0].length; j++){
+        output += lake[i][j] + " ";
+      }
+      output += "\n";
+    }
+  return output;
+  }
 
 }
