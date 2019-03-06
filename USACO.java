29572dd2643e5ac05e81d9dc8ca6891419080925
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-06 11:45:02 -0500

    created stomp method

--- USACO.java
+++ USACO.java
@@ -26,11 +26,20 @@ public class USACO{
     String output = "";
     for(int i = 0; i < instructions.size(); i++){
       for(int j = 0; j < instructions.get(0).size(); j++){
-        output += instructions.get(i).get(j) + " ";
-      }
-      output += "\n";
-    }
-  return output;
-  }
+        output += instructions.get(i).get(j) + " ";}
+      output += "\n";}
+  return output;}
+
+  public void stomp(int ins){
+    int[] herd = new int[9];
+    int counter = 0;
+    for(int i = 0; i < 3; i++){
+      for(int j = 0; j < 3; j++){
+        herd[counter] = lake[instructions.get(ins).get(0)][instructions.get(ins).get(0)];}}
+    Collections.sort(herd);
+    herd[0] -= instructions.get(ins).get(2);
+    for(int i = 0; i < herd.length; i++){
+      if(herd[i] >= herd[0]){
+        herd[i] = herd[0];}}}
 
 }
