db57ea1f041009bcacff9d57f29182c81e9dc015
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-07 18:33:30 -0500

    I have the basic algorithm down
    For the next grid, the grid pushes the ones further out and then  every square(except for the center) that has two of its orthogonal neighbors become the sum of those two numbers.

--- Silver.java
+++ Silver.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.Collections;
 public class Silver{
+  public int[][] ways;
   public String[][] field;
   public int time;
   public int[] start = new int[2];
@@ -12,6 +13,7 @@ public class Silver{
     File file = new File(text);
     Scanner scan = new Scanner(file);
     field = new String[scan.nextInt()][scan.nextInt()];
+    ways = new int[field.length][field[0].length];
     time = scan.nextInt();
     scan.nextLine();
     String output = "";
@@ -24,6 +26,46 @@ public class Silver{
     end[0] = scan.nextInt();
     end[1] = scan.nextInt();}
 
+  public void expand(int n){
+    if(n == 1){
+      ways[start[0]][start[1]] = 1;}
+    if (n == 2){
+      ways[start[0]][start[1]] = 0;
+      ways[start[0] + 1][start[1]] = 1;
+      ways[start[0]][start[1] + 1] = 1;
+      ways[start[0] - 1][start[1]] = 1;
+      ways[start[0]][start[1] - 1] = 1;}
+    else{
+      int[][] newWay = new int[ways.length][ways[0].length];
+      for(int i = 0; i < ways.length; i++){
+        for(int j = 0; j < ways[0].length; j++){
+          try{
+          if(ways[i - 1][j] != 0 && ways[i][j - 1] != 0){
+            newWay[i][j] = ways[i - 1][j] + ways[i][j - 1];}}
+          catch(IndexOutOfBoundsException e){}
+          try{
+          if(ways[i][j + 1] != 0 && ways[i - 1][j] != 0){
+            newWay[i][j] = ways[i][j + 1] + ways[i - 1][j];}}
+          catch(IndexOutOfBoundsException e){}
+          try{
+          if(ways[i + 1][j] != 0 && ways[i][j + 1] != 0){
+            newWay[i][j] = ways[i + 1][j] + ways[i][j + 1];}}
+          catch(IndexOutOfBoundsException e){}
+          try{
+          if(ways[i + 1][j] != 0 && ways[i][j - 1] != 0){
+            newWay[i][j] = ways[i + 1][j] + ways[i][j - 1];}}
+          catch(IndexOutOfBoundsException e){}}}
+        try{
+        newWay[start[0] + n][start[1]] = 1;
+        newWay[start[0]][start[1] + n] = 1;
+        newWay[start[0] - n][start[1]] = 1;
+        newWay[start[0]][start[1] - n] = 1;}
+        catch(IndexOutOfBoundsException e){}
+        for(int i = 0; i < ways.length; i++){
+          for(int j = 0; j < ways[0].length; j++){
+            ways[i][j] = newWay[i][j];}}}
+
+  }
   public String toString(){
     String output = "";
     for(int i = 0; i < field.length; i++){
@@ -31,4 +73,5 @@ public class Silver{
         output += field[i][j] + " ";}
       output += "\n";}
   return output;}
+
 }
