7cd5922f5887464f2f64d9bda31cda28c1c0df7e
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-07 11:45:32 -0500

    started on info method few errors

---
+++ Silver.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+public class Silver{
+  public String[][] field;
+  public int time;
+  public int[] start = new int[2];
+  public int[] end = new int[2];
+  public void info(String text) throws FileNotFoundException{
+    File file = new File(text);
+    Scanner scan = new Scanner(file);
+    field = new String[scan.nextInt()][scan.nextInt()];
+    time = scan.nextInt();
+    String output = "";
+    for(int i = 0; i < field.length; i++){
+      output = scan.nextLine();
+      for(int j = 0; j < field[0].length; j++){
+        field[i][j] = output.substring(j, j + 1);}}
+    start[0] = scan.nextInt();
+    start[1] = scan.nextInt();
+    end[0] = scan.nextInt();
+    end[1] = scan.nextInt();}
+
+  public String toString(){
+    String output = "";
+    for(int i = 0; i < field.length; i++){
+      for(int j = 0; j < field[0].length; j++){
+        output += field[i][j] + " ";}
+      output += "\n";}
+  return output;}
+}
