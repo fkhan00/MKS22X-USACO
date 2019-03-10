f4406160eff57335fb0b764fa1b9c0f24ba5365e
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-03-10 09:12:13 -0400

    completed lab and tested using Bryant Stringham's driver

--- USACO.java
+++ USACO.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.Collections;
 public class USACO{
-  public class bronze{
-  public int[][] lake;
-  public ArrayList<ArrayList<Integer>> instructions;
-  public int finalDep;
-  public int multiplier;
-  public void info(String name) throws FileNotFoundException{
+  public static int[][] lake;
+  public static ArrayList<ArrayList<Integer>> instructions;
+  public static int finalDep;
+  public static int multiplier;
+  public static void info(String name) throws FileNotFoundException{
     File file = new File(name);
     Scanner text = new Scanner(file);
-    String output = text.nextLine();
-    lake = new int[Integer.parseInt(output.substring(0, 1))][Integer.parseInt(output.substring(2, 3))];
-    finalDep = Integer.parseInt(output.substring(4, 6));
-    multiplier = Integer.parseInt(output.substring(7, 8));
+    //String output = text.nextLine();
+    lake = new int[text.nextInt()][text.nextInt()];
+    finalDep = text.nextInt();
+    multiplier = text.nextInt();
+    //lake = new int[Integer.parseInt(output.substring(0, 1))][Integer.parseInt(output.substring(2, 3))];
+    //finalDep = Integer.parseInt(output.substring(4, 6));
+    //multiplier = Integer.parseInt(output.substring(7, 8));
     int counter = 0;
     while(counter < lake.length){
       for(int i = 0; i < lake[0].length; i++){
@@ -28,15 +30,8 @@ public class USACO{
       for(int i = 0; i < 3; i++){
         instructions.get(instructions.size() - 1).add(text.nextInt());}}
   }
-  public String toString(){
-    String output = "";
-    for(int i = 0; i < lake.length; i++){
-      for(int j = 0; j < lake[0].length; j++){
-        output += lake[i][j] + " ";}
-      output += "\n";}
-  return output;}
 
-  public void stomp(int ins){
+  public static void stomp(int ins){
     ArrayList<Integer> herd = new ArrayList<Integer>();
     for(int i = 0; i < 3; i++){
       for(int j = 0; j < 3; j++){
@@ -52,7 +47,7 @@ public class USACO{
       for(int j = 0; j < 3; j++){
         lake[instructions.get(ins).get(0) + i - 1][instructions.get(ins).get(1) + j - 1] = herd.get(counter);
         counter ++;}}}
-    public int bronze(String text)throws FileNotFoundException{
+    public static int bronze(String text)throws FileNotFoundException{
       info(text);
       for(int i = 0; i < instructions.size(); i++){
         stomp(i);}
@@ -61,16 +56,15 @@ public class USACO{
         for(int j = 0; j < lake[0].length; j++){
           if(lake[i][j] < finalDep){
             sum += finalDep - lake[i][j];}}}
-      return sum * 36 * 36 * multiplier * multiplier;}}
-  public class silver{
-        public int[][] ways;
-        public String[][] field;
-        public int time;
-        public int[] start = new int[2];
-        public int[] end = new int[2];
-        public int movesX[] = {1, -1, 0, 0};
-        public int movesY[] = {0, 0, -1, 1 };
-        public void info(String text) throws FileNotFoundException{
+      return sum * 72 * 72;}
+        public static int[][] ways;
+        public static String[][] field;
+        public static int time;
+        public static int[] start = new int[2];
+        public static int[] end = new int[2];
+        public static int movesX[] = {1, -1, 0, 0};
+        public static int movesY[] = {0, 0, -1, 1 };
+        public static void inform(String text) throws FileNotFoundException{
           File file = new File(text);
           Scanner scan = new Scanner(file);
           field = new String[scan.nextInt()][scan.nextInt()];
@@ -87,7 +81,7 @@ public class USACO{
           end[0] = scan.nextInt() - 1;
           end[1] = scan.nextInt() - 1;}
 
-        public void expand(int n){
+        public static void expand(int n){
           if(n == 0){
             ways[start[0]][start[1]] = 1;
             return;}
@@ -105,22 +99,15 @@ public class USACO{
             for(int j = 0; j < ways[0].length; j++){
               ways[i][j] = newWay[i][j];}}}
 
-          public int silver(String text) throws FileNotFoundException{
-            info(text);
+          public static int silver(String text) throws FileNotFoundException{
+            inform(text);
             for(int i = 0; i < time + 1; i++){
               expand(i);}
-            System.out.println(toString());
             return ways[end[0]][end[1]];
           }
-        public String toString(){
-          String output = "";
-          for(int i = 0; i < ways.length; i++){
-            for(int j = 0; j < ways[0].length; j++){
-              output += ways[i][j] + " ";}
-            output += "\n";}
-        return output;}
 
-      }
+
+
 
 
 }
